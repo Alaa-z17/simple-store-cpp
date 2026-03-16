58c9113d6d58db16de55a1ddc8608bc502e7fe08
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-03-16 08:38:46 +0300

    feat: add data types - enums and structs

--- simple-store-cpp/simple-store-cpp.cpp
+++ simple-store-cpp/simple-store-cpp.cpp
@@ -1,20 +1,34 @@
-// simple-store-cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
+enum enCategory { Food = 1, Electronics = 2, Clothing = 3 };
+
+struct stProduct
+{
+    unsigned short ID = 0;
+    string Name = "";
+    float Price = 0;
+    unsigned short Stock = 0;
+    enCategory Category;
+};
+
+struct stCartItem
+{
+    stProduct Product;
+    unsigned short Quantity = 0;
+    float TotalPrice = 0;
+};
+
+struct stInvoice
+{
+    stCartItem Items[100];
+    unsigned short NumberOfItems = 0;
+    float SubTotal = 0;
+    float Discount = 0;
+    float Total = 0;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
